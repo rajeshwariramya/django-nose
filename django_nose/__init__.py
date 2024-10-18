b1bbff62cb64363af799c5b7e44a15a666b6e1d2
rajeshwariramya <103486631+rajeshwariramya@users.noreply.github.com>
2024-10-18 21:06:05 +0530

    Update __init__.py
    Removed unnecessary assert statements.
    Added a default value for __version__ in case the package is not installed.
    Improved the module docstring for clarity.
    Ensured the code adheres to PEP 8 style guidelines.

--- django_nose/__init__.py
+++ django_nose/__init__.py
@@ -1,16 +1,17 @@
 # coding: utf-8
-"""The django_nose module."""
-from pkg_resources import get_distribution, DistributionNotFound
+"""The django_nose module for running tests with Nose in Django."""
 
+from pkg_resources import get_distribution, DistributionNotFound
 from django_nose.runner import BasicNoseRunner, NoseTestSuiteRunner
 from django_nose.testcases import FastFixtureTestCase
 
-assert BasicNoseRunner
-assert NoseTestSuiteRunner
-assert FastFixtureTestCase
+# Import classes
+# No need for assert statements; if the imports fail, an ImportError will be raised.
 
 try:
     __version__ = get_distribution("django-nose").version
 except DistributionNotFound:
-    # package is not installed
-    pass
+    # Package is not installed
+    __version__ = None  # Set version to None if not found
+
+# You can now use BasicNoseRunner, NoseTestSuiteRunner, and FastFixtureTestCase in your code.
