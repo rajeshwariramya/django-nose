d49ed9920763f067371b76f8e8e70e9a8d516b9a
rajeshwariramya <103486631+rajeshwariramya@users.noreply.github.com>
2024-10-19 06:24:15 +0530

    Update setup.py
    Context Managers: Using with open(...) ensures files are closed properly after reading.
    Improved Assertion Handling: Changed assertions to raise ValueError with informative messages if tags are not found, improving debugging.
    String Formatting: Used f-strings for better readability when constructing the long_description.

--- setup.py
+++ setup.py
@@ -10,31 +10,36 @@ def get_long_description(title):
     """Create the long_description from other files."""
     ROOT = os.path.abspath(os.path.dirname(__file__))
 
-    readme = open(os.path.join(ROOT, "README.rst"), "r", "utf8").read()
+    with open(os.path.join(ROOT, "README.rst"), "r", "utf-8") as readme_file:
+        readme = readme_file.read()
+    
     body_tag = ".. Omit badges from docs"
-    readme_body_start = readme.index(body_tag)
-    assert readme_body_start
-    readme_body = readme[readme_body_start + len(body_tag) :]
+    readme_body_start = readme.find(body_tag)
+    if readme_body_start == -1:
+        raise ValueError(f"Body tag '{body_tag}' not found in README.")
+    readme_body = readme[readme_body_start + len(body_tag):]
 
-    changelog = open(os.path.join(ROOT, "changelog.rst"), "r", "utf8").read()
+    with open(os.path.join(ROOT, "changelog.rst"), "r", "utf-8") as changelog_file:
+        changelog = changelog_file.read()
+    
     old_tag = ".. Omit older changes from package"
-    changelog_body_end = changelog.index(old_tag)
-    assert changelog_body_end
+    changelog_body_end = changelog.find(old_tag)
+    if changelog_body_end == -1:
+        raise ValueError(f"Old tag '{old_tag}' not found in changelog.")
     changelog_body = changelog[:changelog_body_end]
 
     bars = "=" * len(title)
     long_description = (
-        """
-%(bars)s
-%(title)s
-%(bars)s
-%(readme_body)s
+        f"""
+{bars}
+{title}
+{bars}
+{readme_body}
 
-%(changelog_body)s
+{changelog_body}
 
 _(Older changes can be found in the full documentation)._
 """
-        % locals()
     )
     return long_description
 
@@ -56,16 +61,6 @@ setup(
     zip_safe=False,
     install_requires=["nose>=1.2.1"],
     test_suite="testapp.runtests.runtests",
-    # This blows up tox runs that install django-nose into a virtualenv,
-    # because it causes Nose to import django_nose.runner before the Django
-    # settings are initialized, leading to a mess of errors. There's no reason
-    # we need FixtureBundlingPlugin declared as an entrypoint anyway, since you
-    # need to be using django-nose to find the it useful, and django-nose knows
-    # about it intrinsically.
-    # entry_points="""
-    #    [nose.plugins.0.10]
-    #    fixture_bundler = django_nose.fixture_bundling:FixtureBundlingPlugin
-    #    """,
     keywords="django nose django-nose",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
